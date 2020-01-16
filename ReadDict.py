8a75e0025b147d4ae156720e42b38524b3ee9e8c
Brent Simpson <simpsonb@waihekehigh.school.nz>
2020-01-17 11:27:54 +1300

    Changes to ReadDict.py

--- ReadDict.py
+++ ReadDict.py
@@ -1,6 +1,24 @@
+#!/usr/bin/env python3
 
-english_terms = open("en.dic").read().splitlines()
+import json
 
-print(english_terms)
+english_terms = open("en.dic").read().splitlines() #opens English dictionary and reads terms into list
 
-print(english_terms[155])
+word_info = open('info.json') #opens JSON file which records the previous word used
+current_word = json.load(word_info)
+prev_word = int(current_word['word_number'])
+
+print(prev_word) #prints the number of the word
+print(english_terms[prev_word]) #Looks up word by index in list and prints
+
+next_item = {}
+next_item["word_number"] = {20}
+
+print next_item
+
+
+with open('info.json', 'w') as f:
+    json.dumps(next_item, f)
+
+
+word_info.close()
